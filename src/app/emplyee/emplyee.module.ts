c575b243fd5601ddca9cd8904f62df991bccfa67
jigneshmahesh07 <jignesh.mahesh07@gmail.com>
2020-01-05 17:33:25 +0530

    Initial commit

---
+++ src/app/emplyee/emplyee.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataComponent } from './data/data.component';
+
+
+
+@NgModule({
+  declarations: [DataComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmplyeeModule { }
